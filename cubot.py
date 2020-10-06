64e587367be05e72bfcaf11d2336f02f30bd1c69
Carson Logston <celogston@gmail.com>
2020-10-06 08:28:24 -0700

    reset cubot and cubot_lib to base servo and control classes

--- cubot.py
+++ cubot.py
@@ -1,14 +1,53 @@
-# the main file to run for operating the CuBot robot
+import cubot_lib
 
-#initialize any variables associated with the robot
-showDebugInfo = false
+#Create an instance, named 'bot', of object 'Control' from 'cubot_lib.py'
+#    Object Control comes pre-initialized with 6 servos and 3 continuous servos
+bot = cubot_lib.Control()
 
-CuBot = cubot_lib.CuBotLib(showDebugInfo)
+#The 6 servos are referenced in an array named 'servo'
+#The 3 continuous servos are referenced in an array named 'cServo'
+#You can assign these pre-defined servos to any variables
+arm1Servo1 = bot.servo[0]
+arm1Servo2 = bot.servo[1]
+arm1cServo = bot.cServo[0]
 
-try:
-  #receive scramble input and initialize camera
+arm2Servo1 = bot.servo[2]
+arm2Servo2 = bot.servo[3]
+arm2cServo = bot.cServo[1]
 
-except Exception, e:
-    print("Error: " + str(e))
+arm3Servo1 = bot.servo[4]
+arm3Servo2 = bot.servo[5]
+arm3cServo = bot.cServo[2]
 
-CuBot.reset()
+#####There are 3 different move functions, listed below:#####
+
+#Function move(int angle, double rate) //from Servo class
+#    'angle' specifies the desired angle that the selected servo must move to (from 0 to 180)
+#    'rate' specifies the amount of time (in seconds) the servo will pause between each angle
+arm1Servo1.move(90,0.0001)
+arm3Servo2.move(54,0.0005)
+
+#Function move(double direction, double period) //from ContinuousServo class
+#    'direction' specifes the rotational direction and speed (-1 to 1)
+#    'period' specifies the period of time, in seconds, the cServo must spin
+arm1cServo.move(1, 1)
+arm2cServo.move(-0.3, 0.5)
+
+#Function move(int angle0, int angle1, int angle2, int angle3, int angle4, int angle5) //from Control class
+#     This function allows servos to move synchronously
+#     Each on these values represent the angle that the respected servo must move to.
+#     move(0, 0, 0, 180, 180, 180) //this statement will move servos 0-2 to 0 degrees and servos 3-5 to 180 degrees
+bot.move(0, 0, 0, 180, 180, 180)
+
+#Always collect your garbage!
+#All servos are set to return to 90 degrees upon deletion
+del bot
+del arm1Servo1
+del arm1Servo2
+del arm2Servo1
+del arm2Servo2
+del arm3Servo1
+del arm3Servo2
+del arm1cServo
+del arm2cServo
+del arm3cServo
--- cubot_lib.py
+++ cubot_lib.py
@@ -1,34 +1,105 @@
-# This code is a library of support functions for the CuBot scrambling robot
+class Servo:
+    'Servo object'
+    
+#    from adafruit_servokit import ServoKit
+#    import adafruit_motor.servo
+    import time
+    
+    def __init__(self, ind, kit, pin, pulseMin, pulseMax, angle):
+        self.index = ind
+#        self.servo = kit.servo[pin]
+#        self.servo.set_pulse_width_range(pulseMin, pulseMax)
+#        self.servo.angle = angle
+        self.currentAngle = angle
+        self.currentDirection = 0
+        print (self.__class__.__name__, "instance", self.index, "created")
+        
+    def __del__(self):
+        self.move(90,0.0001)
+        print (self.__class__.__name__, "instance", self.index, "has been garbage collected")
+        
+    def move(self, angle, rate):
+        while self.currentAngle != angle:
+            if self.currentAngle < angle:
+                self.currentAngle += 1
+#                self.servo.angle = self.currentAngle
+                Servo.time.sleep(rate)
+                
+            else:
+                self.currentAngle -= 1
+#                self.servo.angle = self.currentAngle
+                Servo.time.sleep(rate)
+        print ("Servo", self.index, "moved to angle", self.currentAngle)
 
-class CuBotLib(object):
-#define variables
+class ContinuousServo:
+    'Continuous Servo object'
+    
+#    from adafruit_servokit import ServoKit
+#    import adafruit_motor.servo
+    import time
+    
+    def __init__(self, ind, kit, pin):
+        self.index = ind
+#        self.servo = kit.continuous_servo[pin]
+        print (self.__class__.__name__, "instance", self.index, "created")
+        
+    def __del__(self):
+        print (self.__class__.__name__, "instance", self.index, "has been garbage collected")
+        
+    def move(self, direction, period):
+#        self.servo.throttle = direction
+        Servo.time.sleep(period)
+#        self.servo.throttle = 0
+        if direction > 0:
+            text = "forward"
+        else:
+            text = "backward"
+        print ("Continuous servo", self.index, "moved", text, "for", period, "seconds")
 
-  def debug():
-  #debug issues
+class Control:
+    'Control all servos via I2C communication to PCA9685 controller'
+#    from adafruit_servokit import ServoKit
+#    import adafruit_motor.servo
+    import time
 
-  def reset():
-  #set the robot to a starting position
-
-  def move(self, position):
-  #move the arm over to the position
-
-  def rotate(self, degree):
-  #rotate the arm the specified degrees
-
-  def clamp(self, size):
-  #set the arm's clamp to the given size
-
-  def release(self):
-  #set the arm's clamp to open
-
-  def orient(self, face):
-  #perform a clamp, rotation, and release to show the desired face
-
-  def execute(self, cmd):
-  #execute a movement command
-
-  def read(self):
-  #reads the current cube's face with the camera
-
-  def readcube(self):
-  #reads all of the cube's faces with the camera
+    kit = 0 #mock value for 'kit'
+#    kit = ServoKit(channels = 16)
+    
+    def __init__(self):
+        class_name = self.__class__.__name__
+        print (class_name, "instance created")
+        self.servo = []
+        self.cServo = []
+        self.servo.append(Servo(0, Control.kit, 0, 500, 2500, 90))
+        self.servo.append(Servo(1, Control.kit, 1, 500, 2500, 90))
+        self.servo.append(Servo(2, Control.kit, 3, 500, 2500, 90))
+        self.servo.append(Servo(3, Control.kit, 5, 500, 2500, 90))
+        self.servo.append(Servo(4, Control.kit, 7, 500, 2500, 90))
+        self.servo.append(Servo(5, Control.kit, 9, 500, 2500, 90))
+        self.cServo.append(ContinuousServo(0, Control.kit, 11))
+        self.cServo.append(ContinuousServo(1, Control.kit, 13))
+        self.cServo.append(ContinuousServo(2, Control.kit, 15))
+    
+    def __del__(self):
+        print (self.__class__.__name__, "instance has been garbage collected")
+    
+    def move(self, a0, a1, a2, a3, a4, a5):
+        angle = [a0, a1, a2, a3, a4, a5]
+        for i in range (6):
+            if self.servo[i].currentAngle == angle[i]:
+                self.servo[i].currentDirection = 0
+            if self.servo[i].currentAngle < angle[i]:
+                self.servo[i].currentDirection = 1
+            if self.servo[i].currentAngle > angle[i]:
+                self.servo[i].currentDirection = -1
+        flag = True
+        while flag:
+            flag = False
+            for i in range (6):
+                if self.servo[i].currentDirection != 0:
+                    flag = True
+                    self.servo[i].currentAngle += self.servo[i].currentDirection
+#                    self.servo[i].servo.angle = self.servo[i].currentAngle
+                    if self.servo[i].currentAngle == angle[i]:
+                        self.servo[i].currentDirection = 0
+                        print ("Servo", i, "moved to angle", self.servo[i].currentAngle)
