f998f7dda28cf97642d2ed427d930a3ebc2a582e
Josh Ortiz <37316797+JoshChubi@users.noreply.github.com>
2021-04-30 03:36:55 -0700

    Update Global.py

--- Master/Global.py
+++ Master/Global.py
@@ -1,11 +1,11 @@
 import math
 
-class Face(object): # Since python doens have Enum, made my own as it makes tracking code simpiler
+class Face(object): # Since python doens have Enum, made my own as it makes tracking code cleaner/simpiler
     Front   = 0
     Left    = 1
     Back    = 2
     Right   = 3
-    Up      = 4
+    Up      = 4 #Up no Longer Exist, but keep in for coherency
     Down    = 5
 Face = Face()   # Enum to signify face of Cube
 
@@ -18,7 +18,9 @@ class Dir(object):
     Below   = 5
 Dir = Dir()
 
-GridUnit = 1.0
 
-Dimension = 3
+# May not be needed 
+Dimension = 3   
 Boundary = math.floor(Dimension/2)
+
+F = ["Front","Left","Back","Right","Up","Down"] # IGNORE For internal test use (displaying Sequences)
